dea5afc5e2b6db46eae89c36b15063048d9a8507
Feiyizhan <xmlde@vip.qq.com>
2016-03-23 23:47:55 +0800

    优化普通消息的处理，对消息内容先进行解析，再处理。
    处理的规则
    1）群消息：分解为消息发送成员和消息内容。
    2）对所有的消息内容，过滤html标记。

--- src/main/java/feiyizhan/weixin/msg/handle/text/NormalTextMessageHandle.java
+++ src/main/java/feiyizhan/weixin/msg/handle/text/NormalTextMessageHandle.java
@@ -1,5 +1,8 @@
 package feiyizhan.weixin.msg.handle.text;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import blade.kit.json.JSONObject;
 import feiyizhan.api.tuling.TulingUtil;
 import feiyizhan.weixin.AppControl;
@@ -32,25 +35,48 @@ public class NormalTextMessageHandle extends TextMesageHandle {
 		if(getControl().autoReceiveFlag){ //消息自动答复
 		
 			if(UserUtil.isFoundByID(getControl().AutoReceiveUserList, fromUserID)){  //在自动答复消息清单的，才自动答复
+				List<String> contents = resolveContent(content,fromUserID);
+				if(contents.size()==2){
+					fromUserName = contents.get(0);
+					content = contents.get(1);
+				}else{
+					content = contents.get(0);
+				}
 				
 				String ans = TulingUtil.tuling(fromUserName, content);
 				getSession().webwxsendmsg("【小白机器人】：\n"+ans, fromUserID);
+				
+				
 			}
 		}
 		if(getControl().forwordFlag==true){   //消息转发处理
 			for(int i=0;i<getControl().MangerUsrList.size();i++){
 				JSONObject user =getControl().MangerUsrList.get(i).asObject();
+				List<String> contents = resolveContent(content,fromUserID);
+				if(contents.size()==2){
+					fromUserName = contents.get(0);
+					content = contents.get(1);
+				}else{
+					content = contents.get(0);
+				}
+
 				getSession().webwxsendmsg("【转发消息】：\n"+
 						"From:"+fromUserName+"\n"+
-						"Content:"+content.replace("<br/>", "\n")
+						"Content:"+content
 						,UserUtil.getUserID(user));
-
 			}
 		}
 		
 		if(getControl().keyWorkFlag){ //关键字消息提醒
 			for(int i=0;i<getControl().MangerUsrList.size();i++){
 				JSONObject user =getControl().MangerUsrList.get(i).asObject();
+				List<String> contents = resolveContent(content,fromUserID);
+				if(contents.size()==2){
+					fromUserName = contents.get(0);
+					content = contents.get(1);
+				}else{
+					content = contents.get(0);
+				}
 				for(String str:getControl().keyWordList){
 					if(content.indexOf(str)>=0){
 						getSession().webwxsendmsg(
@@ -65,6 +91,41 @@ public class NormalTextMessageHandle extends TextMesageHandle {
 		
 	
 		return true;
+	}
+
+	/**
+	 * 解析消息，如果是群消息，拆分为群两部分，一部分为群成员名称，一部分为内容。
+	 * @param content
+	 * @param fromUserID
+	 * @return
+	 */
+	private List<String> resolveContent(String content,String fromUserID){
+		List<String> contents = new ArrayList<String>();
+		
+		if(fromUserID.startsWith("@@")){  // 群消息
+			contents = MessageUtil.resolveGroupContent(content);
+			if(contents.size()==2){
+				String val = contents.get(1).replace("<br/>", "\n").replaceAll("<[.[^<]]*>","");
+				String id = contents.get(0);
+				contents.remove(0);
+				contents.remove(1);
+				JSONObject member = getSession().getUserByID(id, fromUserID);
+				if(member!=null){
+					contents.add(UserUtil.getUserRemarkName(member));
+					contents.add(val);
+				}else{
+					contents.add(id);
+					contents.add(val);
+				}
+					
+			}else{
+				contents.add(content.replace("<br/>", "\n").replaceAll("<[.[^<]]*>",""));
+			}
+		}else{
+			contents.add(content.replace("<br/>", "\n").replaceAll("<[.[^<]]*>",""));
+		}
+		
+		return contents;
 	}
 
 }
