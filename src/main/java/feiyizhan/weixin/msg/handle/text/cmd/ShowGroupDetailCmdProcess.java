5abce4d8ab9fc7563d9ce86018af6561bd8feace
Feiyizhan <xmlde@vip.qq.com>
2016-03-29 21:24:54 +0800

    增加指令
    【显示地域报告】:显示地域报告  -- 显示当前群成员地域报告
    【显示未改名的群成员】:显示未改名的群成员  -- 显示当前群成员中未改名的清单
    【显示不符合群名规则的群成员】:显示不符合群名规则的群成员 前缀  -- 显示当前群成员中未按照改名规则的改名的清单（前缀匹配）
    
    删除原【显示明细】指令中的地域数据显示。

--- src/main/java/feiyizhan/weixin/msg/handle/text/cmd/ShowGroupDetailCmdProcess.java
+++ src/main/java/feiyizhan/weixin/msg/handle/text/cmd/ShowGroupDetailCmdProcess.java
@@ -34,7 +34,10 @@ public class ShowGroupDetailCmdProcess extends BaseCmdProcess {
 		StringBuilder sb = new StringBuilder();
 		sb.append("显示当前群明细命令:\n");
 		sb.append("--群聊天模式\n");
-		sb.append("【显示明细】:显示明细  -- 显示当前群明细\n");
+		sb.append("【显示明细】:显示明细  -- 显示当前群成员明细\n");
+		sb.append("【显示地域报告】:显示地域报告  -- 显示当前群成员地域报告\n");
+		sb.append("【显示未改名的群成员】:显示未改名的群成员  -- 显示当前群成员中未改名的清单\n");
+		sb.append("【显示不符合群名规则的群成员】:显示不符合群名规则的群成员 前缀  -- 显示当前群成员中未按照改名规则的改名的清单（前缀匹配）\n");
 		return sb.toString();
 	}
 
@@ -43,8 +46,11 @@ public class ShowGroupDetailCmdProcess extends BaseCmdProcess {
 		// TODO 自动生成的方法存根
 		String[] cmds = content.split("[ ]");
 		String cmd=null;
+		String prefix=null ;  //前缀
+		if(fromUserID.equals(toUserID)) return false;  //自己的聊天窗口不执行
 		if(cmds.length==2){
-			return false;
+			cmd =  cmds[0];
+			prefix = cmds[1];
 		}else if(cmds.length==1){
 			if(!toUserID.startsWith("@@")) return false;
 			cmd = cmds[0];
@@ -58,16 +64,33 @@ public class ShowGroupDetailCmdProcess extends BaseCmdProcess {
 		
 		if("显示明细".equals(cmd)){
 			this.getHandle().getSession().webwxsendmsg(getGroupDetail(this.getHandle().getSession().getGroup(toUserID)),toUserID);
-			return true;	
+			return true;
+		}else if("显示地域报告".equals(cmd))	{
+			this.getHandle().getSession().webwxsendmsg(getGroupRegionDetail(this.getHandle().getSession().getGroup(toUserID)),toUserID);
+			return true;
+		}else if("显示未改名的群成员".equals(cmd))	{
+			this.getHandle().getSession().webwxsendmsg(getGroupDoNotChangeNameDetail(this.getHandle().getSession().getGroup(toUserID)),toUserID);
+			return true;
+		}else if("显示不符合群名规则的群成员".equals(cmd))	{
+			this.getHandle().getSession().webwxsendmsg(getGroupNonConfirmNameDetail(this.getHandle().getSession().getGroup(toUserID),prefix),toUserID);
+			return true;
 		}else{
 			return false;
 		}	
 	}
 
+
+
 	@Override
 	public boolean isProcess(String cmd) {
 		// TODO 自动生成的方法存根
 		if("显示明细".equals(cmd)){
+			return true;
+		}else if("显示地域报告".equals(cmd)){
+			return true;
+		}else if("显示未改名的群成员".equals(cmd)){
+			return true;
+		}else if("显示不符合群名规则的群成员".equals(cmd)){
 			return true;
 		}else{
 			return false;
@@ -81,7 +104,7 @@ public class ShowGroupDetailCmdProcess extends BaseCmdProcess {
 	private String getGroupDetail(JSONObject group){
 		StringBuilder sb = new StringBuilder();
 		sb.append("当前群【");
-		sb.append(UserUtil.getUserRemarkName(group));
+		sb.append(UserUtil.getUserName(group));
 		sb.append("】:\n");
 		JSONArray memberList = UserUtil.getGroupMemberList(group);
 		sb.append("总共有【");
@@ -91,30 +114,64 @@ public class ShowGroupDetailCmdProcess extends BaseCmdProcess {
 		List<String> maleList = new ArrayList<String>();  //男性
 		List<String> femaleList = new ArrayList<String>();  //女性
 		List<String> contactList = new ArrayList<String>();  //通讯录统计
-		Map<String,Integer> regionCountMap =  new TreeMap<String,Integer>();  //区域统计
 		for(JSONValue val:memberList){
 			JSONObject member = val.asObject();
 			if(UserUtil.isFound(this.getHandle().getSession().MemberList, member)){  //该用户在个人通讯录里
-				contactList.add(UserUtil.getUserRemarkName(member));
+				contactList.add(UserUtil.getUserName(member));
 			}
 			int sex = member.getInt("Sex", -1);
 			switch (sex){
 			case 0:{ //不男不女
-				unisexList.add(UserUtil.getUserRemarkName(member));
+				unisexList.add(UserUtil.getUserName(member));
 				break;
 			}
 			case 1:{ //男性
-				maleList.add(UserUtil.getUserRemarkName(member));
+				maleList.add(UserUtil.getUserName(member));
 				break;
 			}
 			case 2:{ //女性
-				femaleList.add(UserUtil.getUserRemarkName(member));
+				femaleList.add(UserUtil.getUserName(member));
 				break;
 			}
 			default:{  //未识别的性别信息
 				LOGGER.info("[*]未识别的性别信息"+member);
 			}
 			}
+			
+
+			
+			
+		}
+		
+		sb.append(" 其中有【"+ maleList.size()+"】位男性。\n");
+		sb.append(" 有【"+ femaleList.size()+"】位女性。\n");
+		sb.append(" 有【"+ unisexList.size()+"】位未设置性别。\n");
+		sb.append("分别是："+unisexList+"\n");
+		sb.append(" 有【"+ contactList.size()+"】位已加为好友。\n");
+		return sb.toString();
+		
+		
+	}
+	
+	/**
+	 * 显示群成员地域明细
+	 * @param group
+	 * @return
+	 */
+	private String getGroupRegionDetail(JSONObject group){
+		StringBuilder sb = new StringBuilder();
+		sb.append("当前群【");
+		sb.append(UserUtil.getUserName(group));
+		sb.append("】:\n");
+		JSONArray memberList = UserUtil.getGroupMemberList(group);
+		String maxName = "";
+		int max =0 ;
+		
+		
+		Map<String,Integer> regionCountMap =  new TreeMap<String,Integer>();  //区域统计
+		for(JSONValue val:memberList){
+			JSONObject member = val.asObject();
+			
 			
 			//统计地域信息
 			String province = member.getString("Province");  //省
@@ -132,20 +189,86 @@ public class ShowGroupDetailCmdProcess extends BaseCmdProcess {
 			}else{
 				regionCount=1;
 			}
+			if(max < regionCount){
+				max = regionCount;
+				maxName = region;
+			}
 			regionCountMap.put(region, regionCount);
 			
 			
 		}
-		
-		sb.append(" 其中有【"+ maleList.size()+"】位男性。\n");
-		sb.append(" 有【"+ femaleList.size()+"】位女性。\n");
-		sb.append(" 有【"+ unisexList.size()+"】位未设置性别。\n");
-		sb.append("分别是："+unisexList+"\n");
-		sb.append(" 有【"+ contactList.size()+"】位已加为好友。\n");
-		sb.append(" 群友的地域分布数据【"+ regionCountMap+"】。\n");
+		sb.append(" 群友的地域在【"+ regionCountMap.size()+"】个区域。。\n");
+		sb.append(" 其中最多的是来自【"+ maxName+"】的，有【"+max+"】个\n");
+		sb.append(" 分别为【"+ regionCountMap+"】。\n");
 		return sb.toString();
 		
 		
 	}
 
+	
+	
+	/**
+	 * 显示未改名的群成员
+	 * @param group
+	 * @return
+	 */
+	private String getGroupDoNotChangeNameDetail(JSONObject group){
+		StringBuilder sb = new StringBuilder();
+		sb.append("当前群【");
+		sb.append(UserUtil.getUserName(group));
+		sb.append("】:\n");
+		JSONArray memberList = UserUtil.getGroupMemberList(group);
+		
+		List<String> displayNames =  new ArrayList<String>();  //区域统计
+		for(JSONValue val:memberList){
+			JSONObject member = val.asObject();
+			
+			
+			//统计名称
+			String displayName = UserUtil.getDisplayName(member)  ;//群里显示的名称
+			if(StringKit.isBlank(displayName)){
+				displayNames.add(UserUtil.getUserName(member));
+			}
+			
+		}
+		sb.append(" 有【"+ displayNames.size()+"】个人未修改名称。\n");
+		sb.append(" 分别是【"+ displayNames+"】。\n");
+		return sb.toString();
+		
+		
+	}
+	
+	/**
+	 * 显示有修改名称，但没有按规则修改名称的群成员
+	 * @param group
+	 * @return
+	 */
+	private String getGroupNonConfirmNameDetail(JSONObject group,String prefix){
+		StringBuilder sb = new StringBuilder();
+		sb.append("当前群【");
+		sb.append(UserUtil.getUserName(group));
+		sb.append("】:\n");
+		JSONArray memberList = UserUtil.getGroupMemberList(group);
+		
+		List<String> displayNames =  new ArrayList<String>();  //区域统计
+		for(JSONValue val:memberList){
+			JSONObject member = val.asObject();
+			
+			
+			//统计名称
+			String displayName = UserUtil.getDisplayName(member)  ;//群里显示的名称
+			if(StringKit.isNotBlank(displayName)){
+				if(!displayName.startsWith("prefix")){
+					displayNames.add(UserUtil.getUserName(member));
+				}
+				
+			}
+			
+		}
+		sb.append(" 有【"+ displayNames.size()+"】个人有修改名称，但没有按规则修改名称。\n");
+		sb.append(" 分别是【"+ displayNames+"】。\n");
+		return sb.toString();
+		
+		
+	}
 }
